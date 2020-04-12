c1e9a583bef71a4943d462bf9cd9eb35bdaae896
Chenille Vachette <qdimarellis@laposte.net>
2020-04-12 10:50:20 +0200

    changed way commands were detected/rejected

--- mybot.js
+++ mybot.js
@@ -14,26 +14,37 @@ client.on("ready", () => {
 client.on("message", (message) => {
 
     if (message.author.bot) return;
-    if (message.content.indexOf(config.prefix) !== 0) return;
-    if (message.content.match(/&\d{1,}d\d{1,}/) == null) {
-      if (message.content.match(/&help/) != null) {
-        message.channel.send("USAGE : &xdy to roll x y-sided dices");
-        return;
-      } else return;
-    };
+    if (message.content.indexOf(config.prefix) !== 0) return; // /&test \d{1,}d\d{1,} \d{1,} .*/
 
-    const args = message.content.slice(config.prefix.length).trim().split(/d/);
+    const args = message.content.slice(config.prefix.length).trim().split(/ /);
     const command = args.shift().toLowerCase();
-    const numberOfRolls = Number(command);
-    const dice          = Number(args.shift().toLowerCase());
+    switch (command) {
+      case "help":
+        message.channel.send("USAGE : &xdy to roll x y-sided dices");
+        break;
+      case "test":
+        break;
 
-    rollArray = [];
-    for (var i=0; i<numberOfRolls; i++) {
-      rollArray.push(randomInt(1,dice));
+      default:
+        if (message.content.match(/&\d{1,}d\d{1,}/) == null) {
+          message.channel.send("This is not a valid command, please enter " + config.prefix + "help for usage guidelines !");
+          return;
+        }
+
+        roll          = command.split(/d/);
+        numberOfRolls = Number(roll.shift());
+        dice          = Number(roll.shift());
+
+        rollArray = [];
+        for (var i=0; i<numberOfRolls; i++) {
+          rollArray.push(randomInt(1,dice+1));
+        }
+    
+        message.channel.send("Rolling " + numberOfRolls + " dice of " + dice);
+        message.channel.send(rollArray.toString());
+        break;
     }
 
-    message.channel.send("Rolling " + numberOfRolls + " dice of " + dice);
-    message.channel.send(rollArray.toString());
   });
 
   client.login(config.token);
